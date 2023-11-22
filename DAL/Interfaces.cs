58402db31090fb9585b3025e3e09414d814bd089
Oleksandr Trukhin <trukhino@ou.org>
2023-11-22 23:23:46 +0100

    init

---
+++ DAL/Interfaces.cs
@@ -0,0 +1,15 @@
+﻿namespace DAL
+{
+    interface IStudy
+    {
+        public string Study();
+    }
+    interface ISing
+    {
+        public string Sing();
+    }
+    interface IRepair
+    {
+        public string Repair();
+    }
+}
